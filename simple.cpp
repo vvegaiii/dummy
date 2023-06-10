33a20e7d5187f908b92e4e9a10ce1ff2b27c6ba3
vvegaiii <vvegaiii@gmail.com>
2023-06-10 14:33:20 -0700

    Updated and corrected errors.

--- simple.cpp
+++ simple.cpp
@@ -3,11 +3,12 @@ using namespace std;
 
 void headerFile(){
   system("clear");
-  cout << "Welcome!\n"
+  cout << "Welcome!\n";
   cout << "========\n" << endl;
+}
   
 int main(){
   headerFile();
   cout << "Hello again--working on Github with VS Code";
-  return 0
+  return 0;
 }
